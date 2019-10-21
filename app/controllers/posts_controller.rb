be977dd8c4a9ec07d74da51d934f9eadc24f2525
Luke Prendergast <luke.prendergast94@gmail.com>
2019-10-21 15:56:38 +0100

    Got to MVP. User can create, edit,update,delete posts. Create page, edit page. create a user.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  before_action :find_post, only:[:show,:edit,:update,:destroy]
+  before_action :check_if_user_has_perms, only: [:edit,:update,:destroy]
+
   def show
-    @post = Post.find(params[:id])
   end
 
   def index
@@ -22,10 +24,31 @@ class PostsController < ApplicationController
   def edit
   end
 
+  def update
+    @post.update(post_params(:title, :content))
+    redirect_to page_path(@post.page)
+  end
+
+  def destroy
+    @page = @post.page
+    @post.destroy
+    redirect_to @page
+  end
+
 
   private
 
   def post_params(*args)
     params.require(:post).permit(*args)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def check_if_user_has_perms
+    unless @post.user == current_user || @post.page.user == current_user
+      redirect_to page_path(@post.page)
+    end
   end
 end
