6be727d47539f29c7b6a53825b4f6fd805030647
Tim Golding <tgolding55@hotmail.com>
2019-10-22 15:47:04 +0100

    added basic validation and error messages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,6 @@
 class Page < ApplicationRecord
-  has_many :posts
+  has_many :posts, :dependent => :destroy
   belongs_to :user
+  validates :title, presence: true, uniqueness: true, length: {minimum: 3}
+  validates :content, presence: true, length: {minimum: 10}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :pages
+  has_many :posts, :dependent => :destroy #A user shouldent be able to be deleted, but this is still probably a good idea
+  has_many :pages, :dependent => :destroy
+  validates :username, presence: true, uniqueness: true, length: {minimum: 3}
 end
