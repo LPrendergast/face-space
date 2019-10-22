c32b7bd3c7710b8ec58406a6c5206054e7e5988a
Tim Golding <tgolding55@hotmail.com>
2019-10-22 16:30:37 +0100

    Added passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :posts, :dependent => :destroy #A user shouldent be able to be deleted, but this is still probably a good idea
   has_many :pages, :dependent => :destroy
   validates :username, presence: true, uniqueness: true, length: {minimum: 3}
+  has_secure_password
 end
